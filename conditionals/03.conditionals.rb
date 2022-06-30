0d25b0dd86574ef5c78f33704b5600fa1de6fa95
seldenleonard <selden222@gmail.com>
2022-06-30 00:52:49 -0700

    solves 03.conditionals.rb

---
+++ conditionals/03.conditionals.rb
@@ -0,0 +1,11 @@
+# 3. Use variables to store two numbers, then write a condition that prints 1 if the numbers are both less than 10, and prints 0 otherwise.
+
+number1 = 9
+
+number2 = 2
+
+if number1 < 10 && number2 < 10
+  p 1
+else
+  p 0
+end
