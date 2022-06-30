a26a6310ed97b8c6e40e79efa538bae199970b5f
seldenleonard <selden222@gmail.com>
2022-06-30 00:55:41 -0700

    solves 04.conditionals.rb

---
+++ conditionals/04.conditionals.rb
@@ -0,0 +1,9 @@
+# 4. Use a variable to store a number, then write a condition that prints 1 if the number is over 9000, and prints -1 otherwise.
+
+number = 12200
+
+if number > 9000
+  p 1
+else
+  p -1
+end
