e1c77eb1c043f1127b87b1d59a4ed3682e1a9da2
seldenleonard <selden222@gmail.com>
2022-06-30 00:49:46 -0700

    solves 02.conditionals.rb

---
+++ conditionals/02.conditionals.rb
@@ -0,0 +1,11 @@
+# 2. Use a variable to store a number, then write a condition that prints -1 if the number is less than 10, prints 1 if the number is greater than 10, and prints 0 if the number is equal to 10.
+
+number = 7
+
+if number < 10
+  p -1
+elsif number > 10
+  p 1
+elsif number == 10
+  p 0
+end
