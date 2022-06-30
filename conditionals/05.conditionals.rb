51cccdfcbb7ac3a6c633a75f9e3206d31d1f36ee
seldenleonard <selden222@gmail.com>
2022-06-30 00:59:42 -0700

    solves 05.conditionals.rb

---
+++ conditionals/05.conditionals.rb
@@ -0,0 +1,13 @@
+# 5. Use a variable to store a number, then write a condition that prints 9 if the number is less than 10, prints 19 if the number is less than 20, prints 29 if the number is less than 30, and prints -1 otherwise (only one print statement should occur).
+
+number = 14
+
+if number < 10
+  p 9
+elsif number < 20
+  p 19
+elsif number < 30
+  p 29
+else
+  p -1
+end
