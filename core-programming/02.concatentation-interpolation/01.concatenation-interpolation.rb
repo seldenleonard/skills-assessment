baa8b619a5cd1ff31e277a0491c7d2299c3e3f43
seldenleonard <selden222@gmail.com>
2022-07-01 00:39:32 -0700

    solves first string concatenation problem

---
+++ core-programming/02.concatentation-interpolation/01.concatenation-interpolation.rb
@@ -0,0 +1,6 @@
+# 1. Write a program that uses variables to store a first and last name, then prints the full name in one line using string concatenation (the + operator).
+
+first = "Selden"
+last = "Leonard"
+
+p first + " " + last
