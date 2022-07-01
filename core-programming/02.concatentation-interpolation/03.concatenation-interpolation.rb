776fd9e756fd914cd79ede54290df071b83ac1b9
seldenleonard <selden222@gmail.com>
2022-07-01 00:45:21 -0700

    solves practice problem

--- core-programming/02.concatentation-interpolation/03.concatenation-interpolation.rb
+++ core-programming/02.concatentation-interpolation/03.concatenation-interpolation.rb
@@ -1,2 +1,10 @@
 # 3. Write a program that asks the user to input a word. If the word is "marco", print "polo".
 
+def marco_polo(word)
+  if word == "marco"
+    p "polo"
+  end
+end
+
+marco_polo("marco")
+marco_polo("hello")
