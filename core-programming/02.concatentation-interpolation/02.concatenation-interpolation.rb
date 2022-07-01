3c686985e660bf81560dd629317670afc47c91cb
seldenleonard <selden222@gmail.com>
2022-07-01 00:42:25 -0700

    solves 2nd concatenation problem

--- core-programming/02.concatentation-interpolation/02.concatenation-interpolation.rb
+++ core-programming/02.concatentation-interpolation/02.concatenation-interpolation.rb
@@ -1,2 +1,6 @@
 # 2. Write a program that uses variables to store a first and last name, then prints the full name in one line using string interpolation (the #{} operator).
 
+first = "Selden"
+last = "Leonard"
+
+p "#{first} #{last}"
