896543ebb06c7cb795fa78e4c5594080fedd0f76
seldenleonard <selden222@gmail.com>
2022-07-01 00:56:01 -0700

    solves problem #5

--- core-programming/02.concatentation-interpolation/05.concatenation-interpolation.rb
+++ core-programming/02.concatentation-interpolation/05.concatenation-interpolation.rb
@@ -1,1 +1,7 @@
 # 5. Write a program that uses variables to store three different colors, then prints out a sentence using the colors with string interpolation (the #{} operator).
+
+color1 = "purple"
+color2 = "yellow"
+color3 = "orange"
+
+p "#{color1.capitalize} is my favorite color, but I do enjoy the #{color2} and #{color3} sky at sunset."
