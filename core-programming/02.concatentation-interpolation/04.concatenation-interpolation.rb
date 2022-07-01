a1b424beca427d6881676d93e60385f1acff1c07
seldenleonard <selden222@gmail.com>
2022-07-01 00:50:59 -0700

    solves practice problem number 4

--- core-programming/02.concatentation-interpolation/04.concatenation-interpolation.rb
+++ core-programming/02.concatentation-interpolation/04.concatenation-interpolation.rb
@@ -1,2 +1,7 @@
 # 4. Write a program that uses variables to store three different colors, then prints out a sentence using the colors with string concatenation (the + operator).
 
+color1 = "red"
+color2 = "white"
+color3 = "blue"
+
+p "The Flag of the United States of America is " + color1 + ", " + color2 + ", and last but not least, " + color3 + "!"
