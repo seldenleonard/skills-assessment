4bdcc2c811b5624a21bbcd3ea9082e0e48f1a8e2
seldenleonard <selden222@gmail.com>
2022-07-01 01:07:02 -0700

    solves built-in method problem #4

--- core-programming/03.built-in-methods/04.built-in-methods.rb
+++ core-programming/03.built-in-methods/04.built-in-methods.rb
@@ -1,2 +1,8 @@
 # 4. Write a program that asks the user to enter a word, then prints that word in reverse order.
 
+def backwards(word)
+  p word.reverse
+end
+
+backwards("slinky")
+backwards("racecar")
