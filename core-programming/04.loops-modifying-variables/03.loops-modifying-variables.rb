f5dc080e28afc1ffed384304e50ee3fe3f01ee75
seldenleonard <selden222@gmail.com>
2022-07-06 00:04:07 -0700

    solves 3rd algorithm

--- core-programming/04.loops-modifying-variables/03.loops-modifying-variables.rb
+++ core-programming/04.loops-modifying-variables/03.loops-modifying-variables.rb
@@ -1,1 +1,9 @@
 # 3. Write a while loop that asks the user to enter a word and will run forever until the user enters the word "stop".
+
+while true
+  puts "Enter a word: "
+  input = gets.chomp
+  if input == "stop"
+    break
+  end
+end
