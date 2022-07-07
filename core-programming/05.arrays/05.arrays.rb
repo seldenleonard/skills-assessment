b72aec061dd818498c25c5491efa1866159ff4b3
seldenleonard <selden222@gmail.com>
2022-07-06 23:44:28 -0700

    solves 5th array problem

--- core-programming/05.arrays/05.arrays.rb
+++ core-programming/05.arrays/05.arrays.rb
@@ -1,1 +1,5 @@
 # 5. Create an array to store 3 strings with lower case letters. Then change the third string to have all capital letters and print the array on one line.
+
+strings = ["hi", "hello", "howdy"]
+strings[2].upcase!
+p strings
