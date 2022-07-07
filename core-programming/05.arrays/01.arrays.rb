999aeaa84945ad25b5d0c89e8c2fa6aa02725888
seldenleonard <selden222@gmail.com>
2022-07-06 23:28:30 -0700

    solves first array problem

--- core-programming/05.arrays/01.arrays.rb
+++ core-programming/05.arrays/01.arrays.rb
@@ -1,1 +1,7 @@
 # 1. Create an array to store 3 words. Then add two more words to the array and print the array on one line.
+
+words = ["hi", "hello", "howdy"]
+
+words << "bonjour"
+p words << "hey"
+
