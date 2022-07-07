57e97469af049edb26230b8fe3edfbb669039f03
seldenleonard <selden222@gmail.com>
2022-07-06 23:37:40 -0700

    solves 3rd array problem

--- core-programming/05.arrays/03.arrays.rb
+++ core-programming/05.arrays/03.arrays.rb
@@ -1,1 +1,8 @@
 # 3. Create an array to store 5 numbers. Then print out each number on separate lines with a while loop.
+
+numbers = [1, 5, 9, 13, 17]
+i = 0
+while i < numbers.length
+  puts numbers[i]
+  i += 1
+end
