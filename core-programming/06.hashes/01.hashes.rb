9c1e3d19e8ce20b79c0d17598ff58d5dd583c0c8
seldenleonard <selden222@gmail.com>
2022-07-07 01:58:52 -0700

    solves first hash problem

--- core-programming/06.hashes/01.hashes.rb
+++ core-programming/06.hashes/01.hashes.rb
@@ -1,1 +1,6 @@
 # 1. Make a hash to store a person's first name, last name, and email address. Then print each attribute on separate lines.
+
+contact = {"First Name" => "Selden", "Last Name" => "Leonard", "Email" => "sl@gmail.com"}
+p contact["First Name"]
+p contact["Last Name"]
+p contact["Email"]
