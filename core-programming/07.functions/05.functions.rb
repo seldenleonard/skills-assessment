d1fd88d50ca954949c8d7ec4222b17b5883df414
seldenleonard <selden222@gmail.com>
2022-07-08 02:24:36 -0700

    solves 5th algorithmin functions problem set

--- core-programming/07.functions/05.functions.rb
+++ core-programming/07.functions/05.functions.rb
@@ -1,1 +1,9 @@
 # 5. Write a function that takes in a string and returns the first letter of the string. Then run the function and print the result.
+
+def first_letter(str)
+  p str[0]
+end
+
+first_letter("nicely")
+first_letter("Swift")
+first_letter("carbon")
