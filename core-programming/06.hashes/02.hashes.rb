71efcbd32cb9134f2e451101042d32fe47b24fb1
seldenleonard <selden222@gmail.com>
2022-07-07 20:04:14 -0700

    solves 2nd hashes problem

--- core-programming/06.hashes/02.hashes.rb
+++ core-programming/06.hashes/02.hashes.rb
@@ -1,1 +1,6 @@
 # 2. Make an array of hashes to store the first name and last name for 3 different people. Then print out the first person's info.
+
+names = [{"First Name" => "Selden", "Last Name" => "Leonard"}, {"First Name" => "Mike", "Last Name" => "Jones"}, {"First Name" => "John", "Last Name" => "Smith"}]
+
+p names[1]["First Name"]
+p names[1]["Last Name"]
