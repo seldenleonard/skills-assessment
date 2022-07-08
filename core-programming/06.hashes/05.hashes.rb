b90855a84a0f6a4fa0af410a998f4aacf160964d
seldenleonard <selden222@gmail.com>
2022-07-08 02:03:15 -0700

    completes 5th hash Algorithm

--- core-programming/06.hashes/05.hashes.rb
+++ core-programming/06.hashes/05.hashes.rb
@@ -1,1 +1,5 @@
 # 5. Make an array of hashes to store the title and author for 3 different books. Then print out the third book's author.
+
+books = [{:Title => "Fahrenheit 451", :Author => "Ray Bradbury"}, {:Title => "1984", :Author => "George Orwell"}, {:Title => "The Handmaid's Tale", :Author => "Margaret Atwood"}]
+
+puts books[2][:Author]
