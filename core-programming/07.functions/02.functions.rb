94fe67e4e49d035b0d682a451b302ea716211749
seldenleonard <selden222@gmail.com>
2022-07-08 02:15:01 -0700

    solves 2nd functions algorithm

--- core-programming/07.functions/02.functions.rb
+++ core-programming/07.functions/02.functions.rb
@@ -1,1 +1,7 @@
 # 2. Write a function that takes in a string and returns the string with all capital letters. Then run the function and print the result.
+
+def all_caps(str)
+  p str.upcase
+end
+
+all_caps("hello")
