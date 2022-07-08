f29470197f4eb908ca1081b690259287b873707e
seldenleonard <selden222@gmail.com>
2022-07-08 00:13:59 -0700

    solves problem #3 for hashes

--- core-programming/06.hashes/03.hashes.rb
+++ core-programming/06.hashes/03.hashes.rb
@@ -1,1 +1,7 @@
 # 3. Make a hash to store prices for 3 different menu items. Then add a new menu item and price and print the hash to see the result.
+
+menu = {"Bread" => 6, "Wine" => 30, "Lobster" => 100}
+
+menu["Steak" => 70]
+
+puts menu
