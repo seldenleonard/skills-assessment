2833e24532bdbd3fb5fb0f3d90b97ed3c3fbe578
seldenleonard <selden222@gmail.com>
2022-07-08 02:11:20 -0700

    solves functions problem set algorithm number 1

--- core-programming/07.functions/01.functions.rb
+++ core-programming/07.functions/01.functions.rb
@@ -1,1 +1,9 @@
 # 1. Write a function that takes in a number and returns the number times two. Then run the function and print the result.
+
+def two_times(int)
+  2.times do
+    p int
+  end
+end
+
+two_times(27)
