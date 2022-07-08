9a7dc1bd41ea561c464c4098167f54cdb8a2e304
seldenleonard <selden222@gmail.com>
2022-07-08 02:18:24 -0700

    solves 3rd functions algorithm

--- core-programming/07.functions/03.functions.rb
+++ core-programming/07.functions/03.functions.rb
@@ -1,2 +1,9 @@
 # 3. Write a function that takes in two numbers and returns the first number subtracted by the second. Then run the function and print the result.
 
+def subtraction(int1, int2)
+  p int1 - int2
+end
+
+subtraction(10, 4)
+subtraction(-3, 8)
+subtraction(-1, -200)
