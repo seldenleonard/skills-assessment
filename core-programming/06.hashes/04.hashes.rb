335a59420530761aa8a32a555f7e9a7e0958dc21
seldenleonard <selden222@gmail.com>
2022-07-08 01:58:55 -0700

    solves hashes problem #4

--- core-programming/06.hashes/04.hashes.rb
+++ core-programming/06.hashes/04.hashes.rb
@@ -1,1 +1,7 @@
 # 4. Make a hash to store a book's title, author, number of pages, and language. Then print each attribute on separate lines.
+
+book = {:Title => "1984", :Author => "George Orwell", :Pages => 312, :Language => "English"}
+
+book.each do |k, v|
+  puts "#{k}: #{v}"
+end
