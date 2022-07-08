829c874f0f7bd69234b39566b5b3a23a5a2db2bf
seldenleonard <selden222@gmail.com>
2022-07-08 02:20:38 -0700

    solves 4th algorithm in functions problem set

--- core-programming/07.functions/04.functions.rb
+++ core-programming/07.functions/04.functions.rb
@@ -1,1 +1,7 @@
 # 4. Write a function that takes in a number and returns the number times itself. Then run the function and print the result.
+
+def self_multiply(int)
+  p int * int
+end
+
+self_multiply(9)
