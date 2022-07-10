d86a3d852684d55db144691faaf18b3f9dd6edf1
seldenleonard <selden222@gmail.com>
2022-07-10 00:14:03 -0700

    solves 3rd array map problem

--- core-programming/08.array-map.rb/03.arrays-map.rb
+++ core-programming/08.array-map.rb/03.arrays-map.rb
@@ -1,2 +1,10 @@
 #  3. Start with an array of hashes and create a new array of string values from each hash's :name key.
   #  For example, [{name: "Alice", age: 27}, {name: "Blane", age: 16}] becomes ["Alice", "Blane"].
+
+info = [{name: "Alice", age: 27}, {name: "Blane", age: 16}]
+names = []
+info.each do |person|
+  names << person[:name]
+end
+
+p names
