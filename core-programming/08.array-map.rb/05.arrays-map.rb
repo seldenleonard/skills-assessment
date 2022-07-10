ee80586c332ad0f12fa98e92d21eb9798cb1ed8c
seldenleonard <selden222@gmail.com>
2022-07-10 00:29:05 -0700

    solves 5th array map problem

--- core-programming/08.array-map.rb/05.arrays-map.rb
+++ core-programming/08.array-map.rb/05.arrays-map.rb
@@ -1,2 +1,11 @@
 #  5. Start with an array of strings and create a new array with each string's length.
   #  For example, ["hello", "goodbye"] becomes [5, 7].
+
+strings_array = ["hello", "goodbye"]
+strings_array_lengths = []
+
+strings_array.map { |string|
+  strings_array_lengths << string.length
+}
+
+p strings_array_lengths
