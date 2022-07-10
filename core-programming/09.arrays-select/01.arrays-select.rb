e20d9e8c2ed38168b4da7fa46090f11822501958
seldenleonard <selden222@gmail.com>
2022-07-10 00:37:25 -0700

    solves 1st arrays select problem with alternate solution using reject method

--- core-programming/09.arrays-select/01.arrays-select.rb
+++ core-programming/09.arrays-select/01.arrays-select.rb
@@ -7,3 +7,11 @@ numbers_under_20 = []
 p numbers_under_20 = numbers.select{ |number|
   number < 30
 }
+
+# ALTERNATE SOLUTION - USING OPPOSITE METHOD
+numbers = [2, 32, 80, 18, 12, 3]
+numbers_under_20 = []
+
+p numbers_under_20 = numbers.reject{ |number|
+  number >= 30
+}
