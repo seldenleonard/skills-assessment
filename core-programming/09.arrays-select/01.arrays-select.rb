d405584b489d15cc014d98814f64db35c1cd68cc
seldenleonard <selden222@gmail.com>
2022-07-10 00:36:05 -0700

    solves arrays select first problem

--- core-programming/09.arrays-select/01.arrays-select.rb
+++ core-programming/09.arrays-select/01.arrays-select.rb
@@ -1,2 +1,9 @@
 #  1. Start with an array of numbers and create a new array with only the numbers less than 20.
   #  For example, [2, 32, 80, 18, 12, 3] becomes [2, 18, 12, 3].
+
+numbers = [2, 32, 80, 18, 12, 3]
+numbers_under_20 = []
+
+p numbers_under_20 = numbers.select{ |number|
+  number < 30
+}
