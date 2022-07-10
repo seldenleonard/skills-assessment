39f7fca05798de9f95a6f58c8f938b74bc6a6fde
seldenleonard <selden222@gmail.com>
2022-07-09 23:52:16 -0700

    solves 2nd array map problem

--- core-programming/08.array-map.rb/02.arrays-map.rb
+++ core-programming/08.array-map.rb/02.arrays-map.rb
@@ -1,2 +1,9 @@
 #  2. Start with an array of strings and create a new array with each string upcased.
   #  For example, ["hello", "goodbye"] becomes ["HELLO", "GOODBYE"].
+
+strings_array = ["hello", "goodbye"]
+caps_strings_array = strings_array.each do |string|
+  string.upcase!
+end
+
+p caps_strings_array
