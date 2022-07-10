2628b813101ca922bab07c49720253a8f13b947d
seldenleonard <selden222@gmail.com>
2022-07-10 00:24:09 -0700

    creates alternate solution for 4th array map problem

--- core-programming/08.array-map.rb/04.array-map.rb
+++ core-programming/08.array-map.rb/04.array-map.rb
@@ -1,11 +1,20 @@
 #  4. Start with an array of numbers and create a new array with each number plus 7.
   #  For example, [1, 2, 3] becomes [8, 9, 10]
 
-numbers = [1, 2, 3]
+# numbers = [1, 2, 3]
+# numbers_plus_7 = []
+
+# numbers.each do |number|
+#   numbers_plus_7 << number + 7
+# end
+
+# p numbers_plus_7
+
+# ALTERNATE Solution
 numbers_plus_7 = []
 
-numbers.each do |number|
-  numbers_plus_7 << number + 7
-end
+numbers = [1, 2, 3].map { |number|
+  numbers_plus_7.push(number + 7)
+}
 
 p numbers_plus_7
