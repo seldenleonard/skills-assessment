79c275d6972754f9fc771260fe9a2cc39af4a0fa
seldenleonard <selden222@gmail.com>
2022-07-10 00:09:17 -0700

    creates file and adds instructions for 3rd array mapping problem

---
+++ core-programming/08.array-map.rb/03.arrays-map.rb
@@ -0,0 +1,2 @@
+#  3. Start with an array of hashes and create a new array of string values from each hash's :name key.
+  #  For example, [{name: "Alice", age: 27}, {name: "Blane", age: 16}] becomes ["Alice", "Blane"].
