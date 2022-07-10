4724d2a3e2c0dfc98ed5739dd9db42afb020590e
seldenleonard <selden222@gmail.com>
2022-07-09 23:47:56 -0700

    solves first array mapping problem

--- core-programming/08.array-map.rb/01.arrays-map.rb
+++ core-programming/08.array-map.rb/01.arrays-map.rb
@@ -1,3 +1,12 @@
 #  1. Start with an array of numbers and create a new array with each number times 3.
   #  For example, [1, 2, 3] becomes [3, 6, 9].
 
+def times3(numbers)
+  numbers_times3 = []
+  numbers.each do |number|
+    numbers_times3 << number * 3
+  end
+  numbers_times3
+end
+
+p times3([1, 2, 3])
