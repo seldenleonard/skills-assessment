f97129892c94b8e0b2840758072952bfe81d0c7b
seldenleonard <selden222@gmail.com>
2022-07-11 00:13:26 -0700

    solves 5th arrays select problem

--- core-programming/09.arrays-select/05.arrays-select.rb
+++ core-programming/09.arrays-select/05.arrays-select.rb
@@ -1,2 +1,7 @@
 #  5. Start with an array of strings and create a new array with only the strings shorter than 4 letters.
   #  For example, ["a", "man", "a", "plan", "a", "canal", "panama"] becomes ["a", "man", "a", "a"].
+
+strings_array = ["a", "man", "a", "plan", "a", "canal", "panama"]
+p short_strings_array = strings_array.select{ |string|
+  string.length < 4
+}
