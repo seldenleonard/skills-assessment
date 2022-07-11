cb00d59d6baee9759b933b5c47f32bd7f5de9b34
seldenleonard <selden222@gmail.com>
2022-07-11 00:08:38 -0700

    solves 4th arrays select problem

--- core-programming/09.arrays-select/04.arrays-select.rb
+++ core-programming/09.arrays-select/04.arrays-select.rb
@@ -1,2 +1,7 @@
 #  4. Start with an array of numbers and create a new array with only the even numbers.
   #  For example, [2, 4, 5, 1, 8, 9, 7] becomes [2, 4, 8].
+
+numbers_array = [2, 4, 5, 1, 8, 9, 7]
+p even_numbers_array = numbers_array.select{ |number|
+  number.even?
+}
