416d8a36f2da0c05c572b7b0513350746f50a7cd
seldenleonard <selden222@gmail.com>
2022-07-11 00:11:50 -0700

    adds instructions

---
+++ core-programming/09.arrays-select/05.arrays-select.rb
@@ -0,0 +1,2 @@
+#  5. Start with an array of strings and create a new array with only the strings shorter than 4 letters.
+  #  For example, ["a", "man", "a", "plan", "a", "canal", "panama"] becomes ["a", "man", "a", "a"].
