bc672a93f0abe778372dc678bcf48f3f2368106d
seldenleonard <selden222@gmail.com>
2022-07-11 00:01:35 -0700

    solves second arrays select problem.rb

--- core-programming/09.arrays-select/02.arrays-select.rb
+++ core-programming/09.arrays-select/02.arrays-select.rb
@@ -1,2 +1,7 @@
 #  2. Start with an array of strings and create a new array with only the strings that start with the letter "w".
   #  For example, ["winner", "winner", "chicken", "dinner"] becomes ["winner", "winner"].
+
+strings_array = ["winner", "winner", "chicken", "dinner"]
+p w_strings_array = strings_array.select{ |string|
+  string[0] == "w"
+}
