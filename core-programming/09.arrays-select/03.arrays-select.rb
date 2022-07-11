5def333c98091d70bf755f6a1b7ef2ed28eb6608
seldenleonard <selden222@gmail.com>
2022-07-10 23:58:18 -0700

    adds instructions

---
+++ core-programming/09.arrays-select/03.arrays-select.rb
@@ -0,0 +1,2 @@
+#  3. Start with an array of hashes and create a new array with only the hashes with prices greater than 5 (from the :price key).
+  #  For example, [{name: "chair", price: 100}, {name: "pencil", price: 1}, {name: "book", price: 4}] becomes [{name: "chair", price: 100}].
