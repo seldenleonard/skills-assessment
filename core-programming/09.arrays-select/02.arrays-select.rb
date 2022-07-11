958c1134fe0309587be7baeeb4cc322c209d52bd
seldenleonard <selden222@gmail.com>
2022-07-10 23:57:26 -0700

    adds instructions

---
+++ core-programming/09.arrays-select/02.arrays-select.rb
@@ -0,0 +1,2 @@
+#  2. Start with an array of strings and create a new array with only the strings that start with the letter "w".
+  #  For example, ["winner", "winner", "chicken", "dinner"] becomes ["winner", "winner"].
