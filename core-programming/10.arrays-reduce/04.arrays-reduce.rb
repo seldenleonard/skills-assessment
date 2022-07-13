dce7cd0f6feeb3fc26ed486c2912eeee2669f02e
seldenleonard <selden222@gmail.com>
2022-07-12 19:35:15 -0700

    solves 4th arrays reduce problem

--- core-programming/10.arrays-reduce/04.arrays-reduce.rb
+++ core-programming/10.arrays-reduce/04.arrays-reduce.rb
@@ -1,2 +1,16 @@
 #  4. Start with an array of numbers and compute the the minumum number.
   #  For example, [5, 10, 8, 3, 9] becomes 3.
+
+def find_min(numbers)
+  minimum = numbers[0]
+  i = 0
+  while i < numbers.length
+    if numbers[i] < minimum
+      minimum = numbers[i]
+    end
+    i += 1
+  end
+  minimum
+end
+
+p find_min([5, 10, 8, 3, 9])
