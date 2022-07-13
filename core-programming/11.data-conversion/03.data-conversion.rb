ccf683829ae61fc0f4ff63fb3b28ef35714905a3
seldenleonard <selden222@gmail.com>
2022-07-13 02:29:42 -0700

    creates file and adds instructions for 3rd data conversion problem

---
+++ core-programming/11.data-conversion/03.data-conversion.rb
@@ -0,0 +1,2 @@
+#  3. Convert a string into a hash with keys for each letter in the string and values for the number of times the letter appears in the string.
+  #  For example, "bookkeeper" becomes {"b" => 1, "o" => 2, "k" => 2, "e" => 3, "p" => 1, "r" => 1}.
