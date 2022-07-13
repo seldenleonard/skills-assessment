87142770d148ff9717ba5cadbe7cad6c3cb6cd41
seldenleonard <selden222@gmail.com>
2022-07-13 02:15:54 -0700

    solves 1st data conversion problem

--- core-programming/11.data-conversion/01.data-conversion.rb
+++ core-programming/11.data-conversion/01.data-conversion.rb
@@ -1,2 +1,6 @@
 #  1. Convert an array of arrays into a hash.
-  #  For example, [[1, 3], [8, 9], [2, 16]] becomes {1 => 3, 8 => 9, 2 => 16}.
+  #  For example, [[1, 3], [8, 9], [2, 16]] becomes {1 => 3, 8 => 9, 2 => 16}
+
+arrays_array = [[1, 3], [8, 9], [2, 16]]
+
+p arrays_array.to_h
