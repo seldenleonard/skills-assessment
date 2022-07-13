f2e6c235e4b4222c0fa5d19975d3d479f922362d
seldenleonard <selden222@gmail.com>
2022-07-13 02:53:19 -0700

    solves 5th data conversion problem

--- core-programming/11.data-conversion/05.data-conversion.rb
+++ core-programming/11.data-conversion/05.data-conversion.rb
@@ -1,2 +1,12 @@
 #  5. Convert a hash into an array of hashes using the keys from each hash as the :id key in each of the array's hashes.
-  #  For example, {321 => {name: "Alice", age: 31}, 322 => {name: "Maria", age: 27}} becomes [{id: 321, name: "Alice", age: 31}, {id: 322, name: "Maria", age: 27}].
+  #  For example, {321 => {name: "Alice", age: 31}, 322 => {name: "Maria", age: 27}} becomes [{id: 321, name: "Alice", age: 31}, {id: 322, name: "Maria", age: 27}]
+
+people = {321 => {name: "Alice", age: 31}, 322 => {name: "Maria", age: 27}}
+people_array = []
+
+people.each do |id, person|
+  person[:id] = id
+  people_array << person
+end
+
+p people_array
