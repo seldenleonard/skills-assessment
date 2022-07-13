30f8e021761be8f90cc9f2aa6b4fc73717206ab4
seldenleonard <selden222@gmail.com>
2022-07-12 19:06:59 -0700

    solves first arrays reduce problem

--- core-programming/10.arrays-reduce/01.arrays-reduce.rb
+++ core-programming/10.arrays-reduce/01.arrays-reduce.rb
@@ -1,2 +1,6 @@
 #  1. Start with an array of numbers and compute the sum of all the numbers.
   #  For example, [5, 10, 8, 3] becomes 26.
+
+numbers_array = [5, 10, 8, 3]
+
+p sum = numbers_array.sum
--- core-programming/10.arrays-reduce/03.arrays-reduce.rb
+++ core-programming/10.arrays-reduce/03.arrays-reduce.rb
@@ -1,2 +1,3 @@
 #  3. Start with an array of hashes and compute the sum of the prices (from the :price key).
   #  For example, [{name: "chair", price: 100}, {name: "pencil", price: 1}, {name: "book", price: 4}] becomes 105.
+
