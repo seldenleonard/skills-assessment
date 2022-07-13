45badaff110893d98df53dfbf094a71ee5936e56
seldenleonard <selden222@gmail.com>
2022-07-13 02:17:44 -0700

    adds instructions

---
+++ core-programming/11.data-conversion/02.data-conversion.rb
@@ -0,0 +1,2 @@
+#  2. Convert an array of hashes into a hash using the :id key from the array's hashes as the keys in the new hash.
+  #  For example, [{id: 1, color: "blue", price: 32}, {id: 2, color: "red", price: 12}] becomes {1 => {id: 1, color: "blue", price: 32}, 2 => {id: 2, color: "red", price: 12}}.
