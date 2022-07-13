0e96e1efe7663ca6d6b1ad8bc39ec8dd46036258
seldenleonard <selden222@gmail.com>
2022-07-12 19:25:43 -0700

    solves 3rd arrays reduce problem

--- core-programming/10.arrays-reduce/03.arrays-reduce.rb
+++ core-programming/10.arrays-reduce/03.arrays-reduce.rb
@@ -1,3 +1,11 @@
 #  3. Start with an array of hashes and compute the sum of the prices (from the :price key).
   #  For example, [{name: "chair", price: 100}, {name: "pencil", price: 1}, {name: "book", price: 4}] becomes 105.
 
+hashes_array = [{name: "chair", price: 100}, {name: "pencil", price: 1}, {name: "book", price: 4}]
+sum = 0
+
+hashes_array.each do |item|
+  sum += item[:price]
+end
+
+p sum
