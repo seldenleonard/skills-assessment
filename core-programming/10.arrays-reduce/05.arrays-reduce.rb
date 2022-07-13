86d23d7ea7a35efee35e70ffa2cfe513c7637308
seldenleonard <selden222@gmail.com>
2022-07-12 19:39:02 -0700

    solves 5th arrays reduce problem

--- core-programming/10.arrays-reduce/05.arrays-reduce.rb
+++ core-programming/10.arrays-reduce/05.arrays-reduce.rb
@@ -1,2 +1,13 @@
 #  5. Start with an array of strings and compute the total length of all the strings.
   #  For example, ["volleyball", "basketball", "badminton"] becomes 29.
+
+sports = ["volleyball", "basketball", "badminton"]
+lengths_sum = 0
+i = 0
+
+while i < sports.length
+  lengths_sum += sports[i].length
+  i += 1
+end
+
+p lengths_sum
