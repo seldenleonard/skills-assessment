21041f90374dbd9d552ba1e9243df548fe27645c
seldenleonard <selden222@gmail.com>
2022-07-12 19:15:09 -0700

    solves 2nd arrays reduce problem

--- core-programming/10.arrays-reduce/02.arrays-reduce.rb
+++ core-programming/10.arrays-reduce/02.arrays-reduce.rb
@@ -1,2 +1,6 @@
 #  2. Start with an array of strings and combine them all into a single string.
   #  For example, ["volleyball", "basketball", "badminton"] becomes "volleyballbasketballbadminton".
+
+strings_array = ["volleyball", "basketball", "badminton"]
+
+p strings_array.join
