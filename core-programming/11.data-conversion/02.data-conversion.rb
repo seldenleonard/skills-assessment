411a94c050bbcf1995e2de2dc95dcb4d10b8d240
seldenleonard <selden222@gmail.com>
2022-07-13 02:28:12 -0700

    solves 2nd data conversion problem

--- core-programming/11.data-conversion/02.data-conversion.rb
+++ core-programming/11.data-conversion/02.data-conversion.rb
@@ -1,2 +1,13 @@
 #  2. Convert an array of hashes into a hash using the :id key from the array's hashes as the keys in the new hash.
-  #  For example, [{id: 1, color: "blue", price: 32}, {id: 2, color: "red", price: 12}] becomes {1 => {id: 1, color: "blue", price: 32}, 2 => {id: 2, color: "red", price: 12}}.
+  #  For example, [{id: 1, color: "blue", price: 32}, {id: 2, color: "red", price: 12}] becomes {1 => {id: 1, color: "blue", price: 32}, 2 => {id: 2, color: "red", price: 12}}
+
+items = [{id: 1, color: "blue", price: 32}, {id: 2, color: "red", price: 12}]
+i = 0
+items_hash = {}
+
+while i < items.length
+  items_hash[items[i][:id]] = items[i]
+  i += 1
+end
+
+p items_hash
