877f1d55fbaf54498acbbb6c55d4639ef9b178db
seldenleonard <selden222@gmail.com>
2022-07-13 02:38:17 -0700

    refactors 4th data conversion problem

--- core-programming/11.data-conversion/04.data-conversion.rb
+++ core-programming/11.data-conversion/04.data-conversion.rb
@@ -1,6 +1,4 @@
 #  4. Convert a hash into an array of arrays.
   #  For example, {"chair" => 100, "book" => 14} becomes [["chair", 100], ["book", 14]]
 
-items = {"chair" => 100, "book" => 14}
-
-p items.to_a
+p items = {"chair" => 100, "book" => 14}.to_a
