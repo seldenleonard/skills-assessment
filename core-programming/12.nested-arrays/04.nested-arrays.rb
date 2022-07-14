1890d40643f7ce681c164b1fb4925163cca4fc49
seldenleonard <selden222@gmail.com>
2022-07-14 04:09:20 -0700

    turns 4th nested arrays problem's solution into method to allow for any input and make more modular

--- core-programming/12.nested-arrays/04.nested-arrays.rb
+++ core-programming/12.nested-arrays/04.nested-arrays.rb
@@ -1,20 +1,21 @@
 #  4. Use a nested loop to find the largest product of any two different numbers within a given array.
   #  For example, [5, -2, 1, -9, -7, 2, 6] becomes 63
 
-numbers = [5, -2, 1, -9, -7, 2, 6]
-i = 0
-i2 = 0
-greatest_product = 0
-
-while i < numbers.length
-  while i2 < numbers.length
-    if numbers[i] * numbers[i2] > greatest_product
-      greatest_product = numbers[i] * numbers[i2]
+def max_product(numbers)
+  i = 0
+  i2 = 0
+  greatest_product = 0
+  while i < numbers.length
+    while i2 < numbers.length
+      if numbers[i] * numbers[i2] > greatest_product
+        greatest_product = numbers[i] * numbers[i2]
+      end
+      i2 += 1
     end
-    i2 += 1
+    i += 1
+    i2 = i + 1
   end
-  i += 1
-  i2 = i + 1
+  greatest_product
 end
 
-p greatest_product
+p max_product([5, -2, 1, -9, -7, 2, 6])
