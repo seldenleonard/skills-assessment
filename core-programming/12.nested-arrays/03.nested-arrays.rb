92923f9adf337673dc6af164d6e78f68dcb94b15
seldenleonard <selden222@gmail.com>
2022-07-14 03:59:20 -0700

    solves third nested array problem

--- core-programming/12.nested-arrays/03.nested-arrays.rb
+++ core-programming/12.nested-arrays/03.nested-arrays.rb
@@ -1,3 +1,20 @@
 #  3. Use a nested loop with one array of strings to create a new array that contains every combination of each string with every other string in the array.
   #  For example, ["a", "b", "c", "d"] becomes ["ab", "ac", "ad", "ba", "bc", "bd", "ca", "cb", "cd", "da", "db", "dc"].
 
+letters = ["a", "b", "c", "d"]
+letters_combinations = []
+i = 0
+i2 = 0
+
+while i < letters.length
+  while i2 < letters.length
+   unless i == i2
+    letters_combinations << "#{letters[i]}#{letters[i2]}"
+   end
+   i2 += 1
+  end
+  i2 = 0
+  i += 1
+end
+
+p letters_combinations
