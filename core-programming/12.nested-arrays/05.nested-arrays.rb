117d0e9fb3586bef41c95dadc917578b728ae242
seldenleonard <selden222@gmail.com>
2022-07-14 04:14:29 -0700

    solves 5th nested arrays problem

--- core-programming/12.nested-arrays/05.nested-arrays.rb
+++ core-programming/12.nested-arrays/05.nested-arrays.rb
@@ -1,2 +1,18 @@
 #  5. Use a nested loop to compute the sum of all the numbers in an array of number pairs.
   #  For example, [[1, 3], [8, 9], [2, 16]] becomes 39
+
+numbers_arrays = [[1, 3], [8, 9], [2, 16]]
+i = 0
+i2 = 0
+sum = 0
+
+while i < numbers_arrays.length
+  while i2 < numbers_arrays[i].length
+    sum += numbers_arrays[i][i2]
+    i2 += 1
+  end
+  i2 = 0
+  i += 1
+end
+
+p sum
