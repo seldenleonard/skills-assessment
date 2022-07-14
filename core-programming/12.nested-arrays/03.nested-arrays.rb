391135b35cc22a82d6dfb42828af60600aeaa29c
seldenleonard <selden222@gmail.com>
2022-07-14 03:50:42 -0700

    creates file and adds instructions for third nested arrays problem

---
+++ core-programming/12.nested-arrays/03.nested-arrays.rb
@@ -0,0 +1,3 @@
+#  3. Use a nested loop with one array of strings to create a new array that contains every combination of each string with every other string in the array.
+  #  For example, ["a", "b", "c", "d"] becomes ["ab", "ac", "ad", "ba", "bc", "bd", "ca", "cb", "cd", "da", "db", "dc"].
+
