68ae9b5597a73d741dbd3c49cf5730cf3a9d050f
seldenleonard <selden222@gmail.com>
2022-07-14 03:40:51 -0700

    adds instructions

---
+++ core-programming/12.nested-arrays/02.nested-arrays.rb
@@ -0,0 +1,2 @@
+#  2. Use a nested loop with two arrays of strings to create a new array of strings with each string combined.
+  #  For example, ["a", "b", "c"] and ["d", "e", "f", "g"] becomes ["ad", "ae", "af", "ag", "bd", "be", "bf", "bg", "cd", "ce", "cf", "cg"].
