f55b8bc39d437b0d5810386b043c71bd8899f600
seldenleonard <selden222@gmail.com>
2022-07-15 01:45:16 -0700

    refactora nested arrays problem 1 to make it more modular as a method and able to accept any input

--- core-programming/12.nested-arrays/01.nested-arrays.rb
+++ core-programming/12.nested-arrays/01.nested-arrays.rb
@@ -1,18 +1,19 @@
 #  1. Use a nested loop to convert an array of number pairs into a single flattened array.
   #  For example, [[1, 3], [8, 9], [2, 16]] becomes [1, 3, 8, 9, 2, 16].
 
-pairs = [[1, 3], [8, 9], [2, 16]]
-pairs_array = []
-i = 0
-i2 = 0
-
-while i < pairs.length
-  while i2 < pairs[i].length
-    pairs_array << pairs[i][i2]
-    i2 += 1
-  end
+def flatten(pairs)
+  pairs_array = []
+  i = 0
   i2 = 0
-  i += 1
+  while i < pairs.length
+    while i2 < pairs[i].length
+      pairs_array << pairs[i][i2]
+      i2 += 1
+    end
+    i2 = 0
+    i += 1
+  end
+  pairs_array
 end
 
-p pairs_array
+p flatten([[1, 3], [8, 9], [2, 16]])
